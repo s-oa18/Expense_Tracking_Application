7d016b21d996002676787c6747400d64e6de4c0a
AyeyemiOlakunle <157706416+AyeyemiOlakunle@users.noreply.github.com>
2024-04-14 07:22:25 +0100

    Update login_process.php

--- login_process.php
+++ login_process.php
@@ -8,13 +8,14 @@
     } else {
         $email = $_POST['email'];
         $password = $_POST['password'];
-        $sql = "SELECT uid, username FROM users WHERE email='$email' and password='$password'";
+        $sql = "SELECT id, username FROM users WHERE email='$email' and password='$password'";
         $result = mysqli_query($db, $sql);
 
         if(mysqli_num_rows($result) == 1) {
             $row = mysqli_fetch_assoc($result);
             $_SESSION['loggedin'] = true;
             $_SESSION['username'] = $row['username']; // Save username in session
+            $_SESSION['userId'] = $row['id']; // Save user id in session
             header("location: home.php");
         } else {
             echo "Incorrect username or password";
